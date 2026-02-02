5f7dcfd79c64996a1fbe1619f1274ca793dc28c1
Hina-Wajid <U2504335@rts-365.dk>
2026-02-02 09:33:31 +0100

    opgave-1

--- arrays.js
+++ arrays.js
@@ -2,8 +2,14 @@ document.addEventListener("DOMContentLoaded", function() {
     let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny"];
     let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
-    let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
-
+    let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21];
+    let newNumbers = numbersArray.filter((tal)=>tal>20);
+    console.log("Opgave 1", newNumbers)
     // --- Løs opgaverne herunder --- 
 
-}) // ends DOMContentLoaded
+}) // ends DOMContentLoaded''
+
+// Udskriv det nye array i konsollen:
+
+
+
